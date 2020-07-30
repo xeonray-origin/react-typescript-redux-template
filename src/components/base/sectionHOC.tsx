3c6a763aac99b6cb9dd579ef4bf908f624f9ca4d
xeonray-origin <amritendu.bhattacharjee@gmail.com>
2020-07-30 16:57:54 +0530

    standardized HOC

--- src/components/base/sectionHOC.tsx
+++ src/components/base/sectionHOC.tsx
@@ -4,30 +4,12 @@ import { ISectionConfig } from '@types';
 
 const SectionHOC = (props: ISectionConfig) => {
   const { cardConfig } = props;
-  console.log(cardConfig, props);
-  switch (cardConfig.type) {
-    case 'Blogs':
-      return (
-        <div id={'Blogs'}>
-          <Typography variant={'h2'}>{cardConfig.data.title}</Typography>
-          <Typography variant={'h5'}>{cardConfig.data.text}</Typography>
-        </div>
-      );
-    case 'Products':
-      return (
-        <div id={'Products'}>
-          <Typography variant={'h2'}>{cardConfig.data.title}</Typography>
-          <Typography variant={'h5'}>{cardConfig.data.text}</Typography>
-        </div>
-      );
-    case 'HERO_SECTION':
-      return (
-        <div id={'Home'}>
-          <Typography variant={'h2'}>HERO</Typography>
-          <Typography variant={'h5'}>{cardConfig.data.text}</Typography>
-        </div>
-      );
-  }
+  return (
+    <div id={cardConfig.type}>
+      <Typography variant={'h2'}>{cardConfig.data.title}</Typography>
+      <Typography variant={'h5'}>{cardConfig.data.text}</Typography>
+    </div>
+  );
 };
 
 export default SectionHOC;
